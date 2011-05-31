640845b6bbc4ca775763eae5925d0bd8b9aa9a03
Luiz Ribeiro <luizribeiro@gmail.com>
2011-05-31 12:01:00 -0300

    color and size attributes access in GyNode

--- ScriptingAPI/src/org/gephi/scripting/wrappers/GyNode.java
+++ ScriptingAPI/src/org/gephi/scripting/wrappers/GyNode.java
@@ -23,6 +23,7 @@ package org.gephi.scripting.wrappers;
 import org.gephi.graph.api.Graph;
 import org.gephi.graph.api.Node;
 import org.gephi.scripting.util.GyNamespace;
+import org.python.core.Py;
 import org.python.core.PyObject;
 
 /**
@@ -30,9 +31,13 @@ import org.python.core.PyObject;
  * @author Luiz Ribeiro
  */
 public class GyNode extends PyObject {
-    
+
     private Graph graph;
     private Node node;
+    // Hack to get color and size attributes into jythonconsole's auto-completion
+    // TODO: get rid of this ugly hack (:
+    public int color;
+    public float size;
 
     public GyNode(Graph graph, Node node) {
         this.graph = graph;
@@ -46,5 +51,35 @@ public class GyNode extends PyObject {
 
     public Node getNode() {
         return node;
+    }
+
+    @Override
+    public void __setattr__(String name, PyObject value) {
+        if (name.equals("color")) {
+            int color = (Integer) value.__tojava__(Integer.class);
+            float red = ((color >> 16) & 0xFF) / 255.0f;
+            float green = ((color >> 8) & 0xFF) / 255.0f;
+            float blue = (color & 0xFF) / 255.0f;
+            node.getNodeData().setColor(red, green, blue);
+        } else if (name.equals("size")) {
+            float size = (Float) value.__tojava__(Float.class);
+            node.getNodeData().setSize(size);
+        } else {
+            super.__setattr__(name, value);
+        }
+    }
+
+    @Override
+    public PyObject __findattr_ex__(String name) {
+        if (name.equals("color")) {
+            int red = (int) Math.round(node.getNodeData().r() * 255.0f);
+            int green = (int) Math.round(node.getNodeData().g() * 255.0f);
+            int blue = (int) Math.round(node.getNodeData().b() * 255.0f);
+            return Py.java2py(new Integer((red << 16) + (green << 8) + blue));
+        } else if (name.equals("size")) {
+            return Py.java2py(new Float(node.getNodeData().getSize()));
+        } else {
+            return super.__findattr_ex__(name);
+        }
     }
 }
