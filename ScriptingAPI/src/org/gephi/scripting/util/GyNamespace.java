1568e5b6c629def8db561e1d0978d80562128bb5
Luiz Ribeiro <luizribeiro@gmail.com>
2011-08-20 00:25:00 -0300

    Added javadoc to GyNamespace

--- ScriptingAPI/src/org/gephi/scripting/util/GyNamespace.java
+++ ScriptingAPI/src/org/gephi/scripting/util/GyNamespace.java
@@ -41,17 +41,33 @@ import org.python.core.Py;
 import org.python.core.PyStringMap;
 
 /**
+ * Class that implements a namespace in Gython.
+ * 
+ * Namespaces are what Python uses to keep track of variables. Hence, a
+ * namespace is just like a dictionary where the keys are the names of variables
+ * and the dictionary values are the values of those variables.
+ * 
+ * Since namespaces are just dictionaries, this class extends PyStringMap, which
+ * is used as the default namespace on Jython. This class is extended on Gython
+ * to perform extra functionality related to Gephi and Gython itself.
  *
  * @author Luiz Ribeiro
  */
 public final class GyNamespace extends PyStringMap {
 
+    /** String constant that prefixes nodes' variables names */
     public static final String NODE_PREFIX = "v";
+    /** String constant that prefixes edges' variables names */
     public static final String EDGE_PREFIX = "e";
+    /** Main graph's variable name */
     public static final String GRAPH_NAME = "g";
+    /** Visible subgraph's variable name */
     public static final String VISIBLE_NAME = "visible";
+    /** Workspace related to this namespace */
     private Workspace workspace;
+    /** Workspace's graph model */
     private GraphModel graphModel;
+    /** Workspace's attribute model */
     private AttributeModel attributeModel;
 
     public GyNamespace(Workspace workspace) {
@@ -60,22 +76,56 @@ public final class GyNamespace extends PyStringMap {
         this.attributeModel = workspace.getLookup().lookup(AttributeModel.class);
     }
 
+    /**
+     * Returns the workspace's graph model.
+     * @return      the workspace's graph model
+     */
     public GraphModel getGraphModel() {
         return graphModel;
     }
 
+    /**
+     * Returns the workspace related to this namespace.
+     * @return      the workspace related to this namespace
+     */
     public Workspace getWorkspace() {
         return workspace;
     }
 
+    /**
+     * Returns a GyNode wrapper for the node given by the id.
+     * @param id    the node's internal id
+     * @return      GyNode wrapper for the given node
+     */
     public GyNode getGyNode(int id) {
         return (GyNode) __finditem__(NODE_PREFIX + id);
     }
 
+    /**
+     * Returns a GyEdge wrapper for the edge given by the id.
+     * @param id    the edge's internal id
+     * @return      GyEdge wrapper for the given edge
+     */
     public GyEdge getGyEdge(int id) {
         return (GyEdge) __finditem__(EDGE_PREFIX + id);
     }
 
+    /**
+     * Assigns an object to the given key.
+     * 
+     * Before assigning the object to the given key, this method checks if the
+     * key matches any of the reserved variable names. In a positive case, the
+     * method will act accordingly. If the key equals to VISIBLE_NAME, for
+     * example, this method will update the current visible view to the given
+     * subgraph.
+     * 
+     * Also, if the user tries to assign something to the main graph's variable
+     * (or any other reserved variable name), this method will throw an
+     * exception to warn the user.
+     * 
+     * @param key   the key to which the object will be assigned to
+     * @param value the object that will be assigned
+     */
     @Override
     public void __setitem__(String key, PyObject value) {
         // The user shouldn't be able to set any of the variables that match
@@ -122,6 +172,25 @@ public final class GyNamespace extends PyStringMap {
         super.__setitem__(key, value);
     }
 
+    /**
+     * Deletes from the namespace the object assigned to the given key.
+     * 
+     * Before effectively deleting the object from the namespace, this method
+     * checks if the key matches one of the reserved variable names. This is
+     * done for preventing that an unaware user deletes a readonly variable
+     * like, for instance, the main graph.
+     * 
+     * In case the given object is a node or an edge, the reference is removed
+     * from the namespace and the entity is also removed from the graph.
+     * 
+     * Note that deleting an object from the namespace does not guarantee that
+     * the object will be removed from memory. This is the case when there are
+     * multiple references to the object on the local namespace (or anywhere
+     * else). Remember that the object is only removed from the memory when
+     * there are no more references to it when the JVM's garbage collector runs.
+     * 
+     * @param key   the key of the object to be deleted from the namespace
+     */
     @Override
     public void __delitem__(String key) {
         // If the user tries to delete a node's binding from the namespace, it
@@ -153,6 +222,20 @@ public final class GyNamespace extends PyStringMap {
         super.__delitem__(key);
     }
 
+    /**
+     * Performs a lookup for the given key in the namespace.
+     * 
+     * Note that before effectively looking in the namespace's string map, this
+     * method checks if the key matches one of the reserved variable names
+     * (e.g. a node, an edge or the main graph). If the key matches one of the
+     * reserved variable names, a wrapper for the entity is instantiated and
+     * added to the namespace's string map before returning it.
+     * 
+     * In case nothing matches the given key on the namespace, null is returned.
+     * 
+     * @param key   lookup key
+     * @return      the object assigned to the key or null if nothing was found
+     */
     @Override
     public PyObject __finditem__(String key) {
         PyObject ret = super.__finditem__(key);
