5cec7d167a2916d8d9c3faa49659dd05c785713d
André Panisson <panisson@gmail.com>
2012-01-31 19:54:53 +0100

    Revert "Right handling of iterable.doBreak()"
    This reverts commit 87714a4cd569f776c9893c1bb0f0ce813f42057b.

--- StreamingServer/src/org/gephi/streaming/server/impl/GraphBufferedEventHandler.java
+++ StreamingServer/src/org/gephi/streaming/server/impl/GraphBufferedEventHandler.java
@@ -41,14 +41,13 @@ Portions Copyrighted 2011 Gephi Consortium.
  */
 package org.gephi.streaming.server.impl;
 
-import org.gephi.data.attributes.api.AttributeController;
 import org.gephi.graph.api.Graph;
 import org.gephi.graph.api.Node;
 import org.gephi.graph.api.NodeIterable;
 import org.gephi.graph.api.NodeIterator;
 import org.gephi.streaming.api.CompositeGraphEventHandler;
+import org.gephi.streaming.api.GraphUpdaterEventHandler;
 import org.gephi.streaming.api.GraphEventHandler;
-import org.openide.util.Lookup;
 
 /**
  * @author panisson
@@ -56,21 +55,23 @@ import org.openide.util.Lookup;
  */
 public class GraphBufferedEventHandler extends CompositeGraphEventHandler {
     
+    private GraphUpdaterEventHandler updater;
     private GraphWriter graphWriter;
 
     public GraphBufferedEventHandler(Graph graph) {
+        this.updater = new GraphUpdaterEventHandler(graph);
+
         Node firstNode = null;
         NodeIterable iterable = graph.getNodes();
         NodeIterator iterator = iterable.iterator();
-        if (iterator.hasNext()) {
+        if (iterator.hasNext())
             firstNode = iterator.next();
-            iterable.doBreak();
-        }
         if (firstNode!=null && firstNode.getNodeData().getAttributes().getValue("dynamicrange")!=null) {
             this.graphWriter = new DynamicGraphWriter(graph, false);
         } else {
             this.graphWriter = new GraphWriter(graph, true);
         }
+        iterable.doBreak();
     }
     
     @Override
