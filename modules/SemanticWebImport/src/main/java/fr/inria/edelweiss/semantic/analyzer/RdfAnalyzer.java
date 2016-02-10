72d25e77c2a2c24b7bfbc82adca6919f8ea9aaf5
Erwan Demairy <erwan.demairy@inria.fr>
2016-02-10 13:47:50 +0100

    Replaced the e.printStackTrace() occurences by Exceptions.printStackTrace(e). logWindow messages are only one-liners now, in order to make them more readable.

--- modules/SemanticWebImport/src/main/java/fr/inria/edelweiss/semantic/analyzer/RdfAnalyzer.java
+++ modules/SemanticWebImport/src/main/java/fr/inria/edelweiss/semantic/analyzer/RdfAnalyzer.java
@@ -15,7 +15,7 @@ import org.gephi.graph.api.GraphModel;
 import org.gephi.utils.longtask.spi.LongTask;
 import org.gephi.utils.progress.Progress;
 import org.gephi.utils.progress.ProgressTicket;
-
+import org.openide.util.Exceptions;
 
 /**
  * The purpose of this class is to build a semantic graph from the result of a
@@ -26,106 +26,105 @@ import org.gephi.utils.progress.ProgressTicket;
  */
 public class RdfAnalyzer implements LongTask, Runnable {
 
-    private static final Logger logger = Logger.getLogger(RdfAnalyzer.class.getName());
-    private SparqlDriver driver;
-    final GraphModel model;
-    final String sparqlRequest;
-    private PostProcessor postProcessor;
-    private String saveResultName;
-    private String sparqlRequestResult;
-    private ProgressTicket progressTicket;
-    private int fynLevel;
+	private static final Logger logger = Logger.getLogger(RdfAnalyzer.class.getName());
+	private SparqlDriver driver;
+	final GraphModel model;
+	final String sparqlRequest;
+	private PostProcessor postProcessor;
+	private String saveResultName;
+	private String sparqlRequestResult;
+	private ProgressTicket progressTicket;
+	private int fynLevel;
 
-    /**
-     * Constructor.
-     *
-     * @param newModel Model to fill.
-     * @param newSparqlRequest SPARQL request to fill with the model.
-     */
-    public RdfAnalyzer(final GraphModel newModel, final String newSparqlRequest, final int fynLevel) {
-        super();
-        this.model = newModel;
-        this.sparqlRequest = newSparqlRequest;
-        this.fynLevel = fynLevel;
-        postProcessor = new EmptyPostProcessor();
-    }
+	/**
+	 * Constructor.
+	 *
+	 * @param newModel Model to fill.
+	 * @param newSparqlRequest SPARQL request to fill with the model.
+	 */
+	public RdfAnalyzer(final GraphModel newModel, final String newSparqlRequest, final int fynLevel) {
+		super();
+		this.model = newModel;
+		this.sparqlRequest = newSparqlRequest;
+		this.fynLevel = fynLevel;
+		postProcessor = new EmptyPostProcessor();
+	}
 
-    @Override
-    public final void run() {
-        logger.info("Begin: Building the implementation relationships graph.");
-        int waitSeconds = 5;
-        Progress.start(progressTicket, waitSeconds);
+	@Override
+	public final void run() {
+		logger.info("Begin: Building the implementation relationships graph.");
+		int waitSeconds = 5;
+		Progress.start(progressTicket, waitSeconds);
 
-        try {
-            Progress.progress(progressTicket);
-            sparqlRequestResult = driver.sparqlQuery(sparqlRequest);
+		try {
+			Progress.progress(progressTicket);
+			sparqlRequestResult = driver.sparqlQuery(sparqlRequest);
 
-            InputStream rdf = new ByteArrayInputStream(getSparqlRequestResult().getBytes(Charset.forName("UTF-8")));
-            RdfParser parser = new RdfParser(rdf, model, fynLevel);
+			InputStream rdf = new ByteArrayInputStream(getSparqlRequestResult().getBytes(Charset.forName("UTF-8")));
+			RdfParser parser = new RdfParser(rdf, model, fynLevel);
 
-            Progress.progress(progressTicket);
-            parser.parse();
-            logger.log(Level.INFO, "Number of triples parsed: {0}", parser.getTripleNumber());
-        } catch (Exception e) {
-            logger.log(Level.INFO, "error when obtaining the nodes and edges: {0}", e.getMessage());
-	    e.printStackTrace();
-        }
+			Progress.progress(progressTicket);
+			parser.parse();
+			logger.log(Level.INFO, "Number of triples parsed: {0}", parser.getTripleNumber());
+		} catch (Exception e) {
+			logger.log(Level.INFO, "error when obtaining the nodes and edges: {0}", e.getMessage());
+			Exceptions.printStackTrace(e);
+		}
 
-        Progress.progress(progressTicket);
-        try {
-            saveResult(getSparqlRequestResult());
-        } catch (Exception e) {
-            logger.log(Level.INFO, "error when saving the result: {0}", e.getMessage());
-        }
-        Progress.progress(progressTicket);
-        postProcessor.setModel(model);
-        postProcessor.run();
+		Progress.progress(progressTicket);
+		try {
+			saveResult(getSparqlRequestResult());
+		} catch (Exception e) {
+			logger.log(Level.INFO, "error when saving the result: {0}", e.getMessage());
+		}
+		Progress.progress(progressTicket);
+		postProcessor.setModel(model);
+		postProcessor.run();
 
-        logger.info("End: Building the implementation relationships graph.");
-        Progress.finish(progressTicket);
-    }
+		logger.info("End: Building the implementation relationships graph.");
+		Progress.finish(progressTicket);
+	}
 
-    public final void setPostProcessing(final LayoutExamplePostProcessor newPostProcessor) {
-        this.postProcessor = newPostProcessor;
-    }
+	public final void setPostProcessing(final LayoutExamplePostProcessor newPostProcessor) {
+		this.postProcessor = newPostProcessor;
+	}
 
-    public final void setSparqlEngine(final SparqlDriver newDriver) {
-        this.driver = newDriver;
-    }
+	public final void setSparqlEngine(final SparqlDriver newDriver) {
+		this.driver = newDriver;
+	}
 
-    public final SparqlDriver getSparqlEngine() {
-        return this.driver;
-    }
+	public final SparqlDriver getSparqlEngine() {
+		return this.driver;
+	}
 
-    public void setSaveResult(String saveResultName) {
-        this.saveResultName = saveResultName;
-    }
+	public void setSaveResult(String saveResultName) {
+		this.saveResultName = saveResultName;
+	}
 
-    private void saveResult(String sparqlRequestResult) throws FileNotFoundException, IOException {
-        if (this.saveResultName.isEmpty()) {
-            return;
-        }
-        FileOutputStream fSave = new FileOutputStream(saveResultName);
-        fSave.write(sparqlRequestResult.getBytes());
-        fSave.close();
-    }
+	private void saveResult(String sparqlRequestResult) throws FileNotFoundException, IOException {
+		if (this.saveResultName.isEmpty()) {
+			return;
+		}
+		FileOutputStream fSave = new FileOutputStream(saveResultName);
+		fSave.write(sparqlRequestResult.getBytes());
+		fSave.close();
+	}
 
-    /**
-     * @return the sparqlRequestResult
-     */
-    public String getSparqlRequestResult() {
-        return sparqlRequestResult;
-    }
+	/**
+	 * @return the sparqlRequestResult
+	 */
+	public String getSparqlRequestResult() {
+		return sparqlRequestResult;
+	}
 
-    @Override
-    public boolean cancel() {
-        return true;
-    }
+	@Override
+	public boolean cancel() {
+		return true;
+	}
 
-    @Override
-    public void setProgressTicket(ProgressTicket pt) {
-        this.progressTicket = pt;
-    }
-
+	@Override
+	public void setProgressTicket(ProgressTicket pt) {
+		this.progressTicket = pt;
+	}
 
 }
--- modules/SemanticWebImport/src/main/java/fr/inria/edelweiss/sparql/RdfParser.java
+++ modules/SemanticWebImport/src/main/java/fr/inria/edelweiss/sparql/RdfParser.java
@@ -15,6 +15,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.gephi.graph.api.Graph;
 import org.gephi.graph.api.GraphModel;
+import org.openide.util.Exceptions;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
@@ -26,198 +27,197 @@ import org.xml.sax.SAXParseException;
  */
 public class RdfParser {
 
-    private static final Logger logger = Logger.getLogger(RdfParser.class.getName());
-    private InputStream fileToParse;
-    private GraphModel modelToFill;
-    private int tripleNumber;
-    int depth = 0;
+	private static final Logger logger = Logger.getLogger(RdfParser.class.getName());
+	private InputStream fileToParse;
+	private GraphModel modelToFill;
+	private int tripleNumber;
+	int depth = 0;
 
-    public RdfParser(final InputStream fileToParse, final GraphModel modelToFill, int depth) {
-        this.fileToParse = fileToParse;
-        this.modelToFill = modelToFill;
-        this.depth = depth;
-    }
+	public RdfParser(final InputStream fileToParse, final GraphModel modelToFill, int depth) {
+		this.fileToParse = fileToParse;
+		this.modelToFill = modelToFill;
+		this.depth = depth;
+	}
 
-    /**
-     * @return the resulting graph.
-     */
-    public final Graph parse() {
-        final ARP arp = new ARP();
+	/**
+	 * @return the resulting graph.
+	 */
+	public final Graph parse() {
+		final ARP arp = new ARP();
 
-        // initialisation - uses ARPConfig interface only.
+		// initialisation - uses ARPConfig interface only.
+		arp.getHandlers().setErrorHandler(new ErrorHandler() {
+			@Override
+			public void fatalError(final SAXParseException e) {
+				logger.log(Level.SEVERE, e.getMessage());
+			}
 
-        arp.getHandlers().setErrorHandler(new ErrorHandler() {
-            @Override
-            public void fatalError(final SAXParseException e) {
-                logger.log(Level.SEVERE, e.getMessage());
-            }
+			@Override
+			public void error(final SAXParseException e) {
+				logger.log(Level.SEVERE, e.getMessage());
+			}
 
-            @Override
-            public void error(final SAXParseException e) {
-                logger.log(Level.SEVERE, e.getMessage());
-            }
+			@Override
+			public void warning(final SAXParseException e) {
+				logger.log(Level.INFO, e.getMessage());
+			}
+		});
+		final ARPParsingHandler statementHandler = new ARPParsingHandler(getModelToFill(), this.getDepth());
+		arp.getHandlers().setStatementHandler(statementHandler);
 
-            @Override
-            public void warning(final SAXParseException e) {
-                logger.log(Level.INFO, e.getMessage());
-            }
-        });
-        final ARPParsingHandler statementHandler = new ARPParsingHandler(getModelToFill(), this.getDepth());
-        arp.getHandlers().setStatementHandler(statementHandler);
+		// parsing.
+		GephiUtils.addAttributeToNodes(GephiUtils.SPARQLID, String.class);
+		try {
+			arp.load(getFileToParse());
+		} catch (SAXException ex) {
+			logger.log(Level.SEVERE, ex.getMessage());
+		} catch (IOException ex) {
+			logger.log(Level.SEVERE, ex.getMessage());
+		}
+		tripleNumber = statementHandler.getTripleNumber();
+		return modelToFill.getGraph();
+	}
 
-        // parsing.
-	GephiUtils.addAttributeToNodes(GephiUtils.SPARQLID,String.class);
-        try {
-            arp.load(getFileToParse());
-        } catch (SAXException ex) {
-            logger.log(Level.SEVERE, ex.getMessage());
-        } catch (IOException ex) {
-            logger.log(Level.SEVERE, ex.getMessage());
-        }
-        tripleNumber = statementHandler.getTripleNumber();
-        return modelToFill.getGraph();
-    }
+	public InputStream getFileToParse() {
+		return fileToParse;
+	}
 
-    public InputStream getFileToParse() {
-        return fileToParse;
-    }
+	public void setFileToParse(final InputStream fileToParse) {
+		this.fileToParse = fileToParse;
+	}
 
-    public void setFileToParse(final InputStream fileToParse) {
-        this.fileToParse = fileToParse;
-    }
+	public GraphModel getModelToFill() {
+		return modelToFill;
+	}
 
-    public GraphModel getModelToFill() {
-        return modelToFill;
-    }
+	public void setModelToFill(final GraphModel modelToFill) {
+		this.modelToFill = modelToFill;
+	}
 
-    public void setModelToFill(final GraphModel modelToFill) {
-        this.modelToFill = modelToFill;
-    }
+	public int getDepth() {
+		return this.depth;
+	}
 
-    public int getDepth() {
-        return this.depth;
-    }
+	public void setDepth(final int depth) {
+		this.depth = depth;
+	}
 
-    public void setDepth(final int depth) {
-        this.depth = depth;
-    }
+	/**
+	 * @return the tripleNumber
+	 */
+	public int getTripleNumber() {
+		return tripleNumber;
+	}
 
-    /**
-     * @return the tripleNumber
-     */
-    public int getTripleNumber() {
-        return tripleNumber;
-    }
+	/**
+	 * Class responsible for converting the rdf file in a graph.
+	 */
+	public static class ARPParsingHandler implements StatementHandler {
 
-    /**
-     * Class responsible for converting the rdf file in a graph.
-     */
-    public static class ARPParsingHandler implements StatementHandler {
+		private GraphModel modelToFill;
+		final transient private GephiUtils gephiUtils;
+		private int tripleNumber;
+		int count;
 
-        private GraphModel modelToFill;
-        final transient private GephiUtils gephiUtils;
-        private int tripleNumber;
-        int count;
+		public ARPParsingHandler(final GraphModel modelToFill, final int count) {
+			setModelToFill(modelToFill);
+			gephiUtils = new GephiUtils(modelToFill);
+			tripleNumber = 0;
+			this.count = count;
+		}
 
-        public ARPParsingHandler(final GraphModel modelToFill, final int count) {
-            setModelToFill(modelToFill);
-            gephiUtils = new GephiUtils(modelToFill);
-            tripleNumber = 0;
-            this.count = count;
-        }
+		@Override
+		public void statement(final AResource nodeSource, final AResource edge, final ALiteral nodeTarget) {
+			addEdge(nodeSource.toString(), edge.toString(), nodeTarget.toString());
+			tripleNumber++;
+		}
 
-        @Override
-        public void statement(final AResource nodeSource, final AResource edge, final ALiteral nodeTarget) {
-            addEdge(nodeSource.toString(), edge.toString(), nodeTarget.toString());
-            tripleNumber++;
-        }
+		@Override
+		public void statement(final AResource nodeSource, final AResource edge, final AResource nodeTarget) {
+			addEdge(nodeSource.toString(), edge.toString(), nodeTarget.toString());
+			tripleNumber++;
+		}
 
-        @Override
-        public void statement(final AResource nodeSource, final AResource edge, final AResource nodeTarget) {
-            addEdge(nodeSource.toString(), edge.toString(), nodeTarget.toString());
-            tripleNumber++;
-        }
+		private void addEdge(final String sourceLabel, final String edgeLabel, final String targetLabel) {
 
-        private void addEdge(final String sourceLabel, final String edgeLabel, final String targetLabel) {
+			String sourceDecoded = GephiUtils.decodeString(sourceLabel);
+			String edgeDecoded = GephiUtils.decodeString(edgeLabel);
+			String targetDecoded = GephiUtils.decodeString(targetLabel);
 
-            String sourceDecoded = GephiUtils.decodeString(sourceLabel);
-            String edgeDecoded = GephiUtils.decodeString(edgeLabel);
-            String targetDecoded = GephiUtils.decodeString(targetLabel);
+			if (GephiExtension.isGephiExtension(edgeDecoded)) {
+				GephiExtension.processGephiExtension(sourceDecoded, edgeDecoded, targetDecoded, gephiUtils);
+			} else {
+				gephiUtils.addNode(sourceDecoded);
+				gephiUtils.addNode(targetDecoded);
+				gephiUtils.addEdge(sourceDecoded, edgeDecoded, targetDecoded);
+				if (count == 0) {
+					return;
+				} else {
+					InputStream followRDF;
+					followRDF = getRDF(targetDecoded.toString());
+					RdfParser temp = new RdfParser(followRDF, modelToFill, --count);
+					temp.parse();
+					return;
+				}
+			}
+		}
 
-            if (GephiExtension.isGephiExtension(edgeDecoded)) {
-                GephiExtension.processGephiExtension(sourceDecoded, edgeDecoded, targetDecoded, gephiUtils);
-            } else {
-                gephiUtils.addNode(sourceDecoded);
-                gephiUtils.addNode(targetDecoded);
-                gephiUtils.addEdge(sourceDecoded, edgeDecoded, targetDecoded);
-                if (count == 0) {
-                    return;
-                } else {
-                    InputStream followRDF;
-                    followRDF = getRDF(targetDecoded.toString());
-                    RdfParser temp = new RdfParser(followRDF, modelToFill, --count);
-                    temp.parse();
-                    return;
-                }
-            }
-        }
+		public GraphModel getModelToFill() {
+			return modelToFill;
+		}
 
-        public GraphModel getModelToFill() {
-            return modelToFill;
-        }
+		final public void setModelToFill(final GraphModel modelToFill) {
+			this.modelToFill = modelToFill;
+		}
 
-        final public void setModelToFill(final GraphModel modelToFill) {
-            this.modelToFill = modelToFill;
-        }
+		public int getTripleNumber() {
+			return tripleNumber;
+		}
 
-        public int getTripleNumber() {
-            return tripleNumber;
-        }
+		public static String sendGetRequest(String endpoint, String requestParameters) {
+			String result = null;
+			if (endpoint.startsWith("http://")) {
+				// Send a GET request to the servlet
+				try {
+					// Construct data
+					StringBuffer data = new StringBuffer();
+					// Send data
+					String urlStr = endpoint;
+					if (requestParameters != null && requestParameters.length() > 0) {
+						urlStr += "?" + requestParameters;
+					}
+					final URL url = new URL(urlStr);
+					final URLConnection conn = url.openConnection();
+					conn.addRequestProperty("Accept", "application/rdf+xml");
+					// Get the response
+					final InputStream inputStream = conn.getInputStream();
+					final BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+					StringBuffer sb = new StringBuffer();
+					String line;
+					while ((line = rd.readLine()) != null) {
+						sb.append(line);
+					}
+					rd.close();
+					result = sb.toString();
+				} catch (Exception e) {
+					Exceptions.printStackTrace(e);
+				}
+			}
+			return result;
+		}
 
-        public static String sendGetRequest(String endpoint, String requestParameters) {
-            String result = null;
-            if (endpoint.startsWith("http://")) {
-                // Send a GET request to the servlet
-                try {
-                    // Construct data
-                    StringBuffer data = new StringBuffer();
-                    // Send data
-                    String urlStr = endpoint;
-                    if (requestParameters != null && requestParameters.length() > 0) {
-                        urlStr += "?" + requestParameters;
-                    }
-                    final URL url = new URL(urlStr);
-                    final URLConnection conn = url.openConnection();
-                    conn.addRequestProperty("Accept", "application/rdf+xml");
-                    // Get the response
-                    final InputStream inputStream = conn.getInputStream();
-                    final BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-                    StringBuffer sb = new StringBuffer();
-                    String line;
-                    while ((line = rd.readLine()) != null) {
-                        sb.append(line);
-                    }
-                    rd.close();
-                    result = sb.toString();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            return result;
-        }
-
-        public static InputStream getRDF(final String strURI) {
-            InputStream result = null;
-            try {
-                URL url = new URL(strURI);
-                URLConnection conn = url.openConnection();
-                conn.addRequestProperty("Accept", "application/rdf+xml");
-                InputStream rdf = new BufferedInputStream(conn.getInputStream());
-                result = rdf;
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return result;
-        }
-    }
+		public static InputStream getRDF(final String strURI) {
+			InputStream result = null;
+			try {
+				URL url = new URL(strURI);
+				URLConnection conn = url.openConnection();
+				conn.addRequestProperty("Accept", "application/rdf+xml");
+				InputStream rdf = new BufferedInputStream(conn.getInputStream());
+				result = rdf;
+			} catch (Exception e) {
+				Exceptions.printStackTrace(e);
+			}
+			return result;
+		}
+	}
 }
