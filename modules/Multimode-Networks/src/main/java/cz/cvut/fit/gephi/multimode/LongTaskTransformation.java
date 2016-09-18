f220395f438b0e8a313d77b69dcce6c24ae3e4b8
Joan <joan.codina@upf.edu>
2016-09-18 23:54:22 +0200

    extension to use cosine similarity

--- modules/Multimode-Networks/src/main/java/cz/cvut/fit/gephi/multimode/LongTaskTransformation.java
+++ modules/Multimode-Networks/src/main/java/cz/cvut/fit/gephi/multimode/LongTaskTransformation.java
@@ -25,18 +25,40 @@ public class LongTaskTransformation implements LongTask, Runnable {
     private String outDimension;
     private boolean removeEdges = true;
     private boolean removeNodes = true;
+    private boolean proportional = true;
+
+    private boolean directed=false;
+    private boolean considerDirected=false;
+    private double threshold=0.0;
+    
     
     private static final int EDGE_TYPE = 1;
     
-    public LongTaskTransformation(Column attributeColumn, String inDimension, String commonDimension, String outDimension, boolean removeEdges, boolean removeNodes) {
+    public LongTaskTransformation(Column attributeColumn,
+            String inDimension, 
+            String commonDimension, 
+            String outDimension,
+            double threshold, 
+            boolean removeEdges, 
+            boolean removeNodes,
+            boolean proportional, 
+            boolean considerDirected) {
+        
         this.attributeColumn = attributeColumn;
         this.inDimension = inDimension;
         this.commonDimension = commonDimension;
         this.outDimension = outDimension;
         this.removeEdges = removeEdges;
         this.removeNodes = removeNodes;
+        this.proportional = proportional;
+        this.considerDirected=considerDirected;
+        this.threshold=threshold;
     }
     
+    public void run_offline(GraphModel graphModel ) {
+           execute(graphModel, false) ;
+    }
+
     @Override
     public void run() {
         // number of tickets
@@ -45,8 +67,16 @@ public class LongTaskTransformation implements LongTask, Runnable {
         // graph
         GraphController graphController = Lookup.getDefault().lookup(GraphController.class);
         GraphModel graphModel = graphController.getGraphModel();
+        execute(graphModel, true) ;
+    }   
+
+        private  void execute(GraphModel graphModel,Boolean online) {
+        // number of tickets
+        if (online) Progress.start(progressTicket, 5);
+
+        // graph
+        GraphController graphController = Lookup.getDefault().lookup(GraphController.class); 
         Graph graph = graphModel.getGraphVisible();
-        
         // Need to do that now because removing all edges later will creates error
         // on undirected graph
         boolean isDirectedGraph = graphModel.isDirected();
@@ -78,21 +108,35 @@ public class LongTaskTransformation implements LongTask, Runnable {
                 secondHorizontal.add(n);
             }
         }
-        
+        if (online) Progress.start(progressTicket, firstVertical.size()+6);
+   
         if (cancelled) {
             return;
         }
-        Progress.progress(progressTicket);
+        if (online) Progress.progress(progressTicket,1);
 
         // first matrix
         Matrix firstMatrix = new Matrix(firstVertical.size(), firstHorizontal.size());
+        Matrix firstUnweightMatrix = new Matrix(firstVertical.size(), firstHorizontal.size());
+        float [] firstWeights= new float[firstVertical.size()]; 
+        float [] firstUnweightWeights= new float[firstVertical.size()]; 
         for (int i = 0; i < firstVertical.size(); i++) {
             Set<Node> intersection = new HashSet<Node>(Arrays.asList(graph.getNeighbors(firstVertical.get(i)).toArray()));
             if (intersection.size() > 0) {
                 try {
                     intersection.retainAll(firstHorizontal);
                     for (Node neighbour : intersection) {
-                        firstMatrix.set(i, firstHorizontal.indexOf(neighbour), 1);
+                     int j=firstHorizontal.indexOf(neighbour);
+                        if (j > -1){
+                            Edge edge = graph.getEdge(firstVertical.get(i), firstHorizontal.get(j));
+                            if (edge!= null) {
+                                double w=edge.getWeight();
+                                firstWeights[i]+=w*w;
+                                firstUnweightWeights[i]+=1;
+                                firstMatrix.set(i, j, w);
+                                firstUnweightMatrix.set(i, j, (float)1.0);
+                        }
+                        }
                     }
                 } catch (UnsupportedOperationException ex) {
                     logger.log(Level.SEVERE, null, ex);
@@ -101,15 +145,28 @@ public class LongTaskTransformation implements LongTask, Runnable {
         }
         // second matrix
         Matrix secondMatrix = new Matrix(secondVertical.size(), secondHorizontal.size());
+        Matrix secondUnweightMatrix = new Matrix(secondVertical.size(), secondHorizontal.size());
+        float [] secondWeights= new float[secondHorizontal.size()];
+        float [] secondUnweightWeights= new float[firstVertical.size()]; 
         for (int i = 0; i < secondVertical.size(); i++) {
             
             Set<Node> intersection = new HashSet<Node>(Arrays.asList(graph.getNeighbors(secondVertical.get(i)).toArray()));
             if (intersection.size() > 0) {
                 try {
                     intersection.retainAll(secondHorizontal);
-                    for (Node neighbour : intersection) {
-                        secondMatrix.set(i, secondHorizontal.indexOf(neighbour), 1);
-                    }
+                         for (Node neighbour : intersection) {
+                            int j=secondHorizontal.indexOf(neighbour);
+                            if (j>-1){                    
+                                Edge edge =graph.getEdge(secondVertical.get(i), secondHorizontal.get(j));
+                                 if (edge!= null) {
+                                    double w=edge.getWeight();
+                                    secondWeights[j]+=w*w;
+                                    secondUnweightWeights[j]+=1;
+                                   secondMatrix.set(i, j, w);
+                                    secondUnweightMatrix.set(i, j, (float)1.0);
+                                }
+                            }
+                         }
                 } catch (UnsupportedOperationException ex) {
                     logger.log(Level.SEVERE, null, ex);
                 }
@@ -118,13 +175,19 @@ public class LongTaskTransformation implements LongTask, Runnable {
         if (cancelled) {
             return;
         }
-        Progress.progress(progressTicket, "Multiplication");
+        if (online) Progress.progress(progressTicket, "Multiplication",2);
         
         Matrix result = firstMatrix.timesParallelIndexed(secondMatrix);
         if (cancelled) {
             return;
         }
-        Progress.progress(progressTicket, "Removing nodes/edges");
+        if (online)  Progress.progress(progressTicket, "Unweighted Multiplication",3);
+        
+        Matrix resultUnw = firstUnweightMatrix.timesParallelIndexed(secondUnweightMatrix);
+        if (cancelled) {
+            return;
+        }
+        if (online) Progress.progress(progressTicket, "Removing nodes/edges",4);
         
         
         if (removeNodes) {
@@ -154,8 +217,8 @@ public class LongTaskTransformation implements LongTask, Runnable {
         if (cancelled) {
             return;
         }
-        Progress.progress(progressTicket, "Creating new edges");
-        /*AttributeController ac = Lookup.getDefault().lookup(AttributeController.class);
+        if (online) Progress.progress(progressTicket, "Creating new edges",5);
+        /* AttributeController ac = Lookup.getDefault().lookup(AttributeController.class);
         AttributeModel model = ac.getModel();        
         AttributeColumn edgeTypeCol = model.getEdgeTable().getColumn("MMNT-EdgeType");        
         if (edgeTypeCol == null) {
@@ -170,26 +233,118 @@ public class LongTaskTransformation implements LongTask, Runnable {
             MMNT = edgeTable.getColumn("MMNT-EdgeType");
         }
         
-        for (int i = 0; i < result.getM(); i++) {
-            for (int j = 0; j < result.getN(); j++) {
-                if (graph.contains(firstVertical.get(i)) && graph.contains(secondHorizontal.get(j)) && graph.getEdge(firstVertical.get(i), secondHorizontal.get(j)) == null && result.get(i, j) > 0) {
-                    Node node1 = firstVertical.get(i);
-                    Node node2 = secondHorizontal.get(j);
-                    if(node1 != node2){
-                        Edge ee = graph.getEdge(node1, node2, EDGE_TYPE);
-                        if(ee == null){ //Add if not already existing
-                            ee = graphModel.factory().newEdge(firstVertical.get(i), secondHorizontal.get(j), EDGE_TYPE, (float) result.get(i, j), isDirectedGraph);
-                            graph.addEdge(ee);
+        Edge ee;
+
+        if(!this.proportional){
+            for (int i = 0; i < result.getM(); i++) {
+                for (int j = 0; j < result.getN(); j++) {
+                    if (graph.contains(firstVertical.get(i)) && graph.contains(secondHorizontal.get(j)) 
+                            && graph.getEdge(firstVertical.get(i), secondHorizontal.get(j)) == null
+                            && result.get(i, j) > threshold) {
+                        Node node1 = firstVertical.get(i);
+                        Node node2 = secondHorizontal.get(j);
+                        if(node1 != node2){
+                             ee = graph.getEdge(node1, node2, EDGE_TYPE);
+                            if(ee == null){ //Add if not already existing
+                                ee = graphModel.factory().newEdge(firstVertical.get(i), secondHorizontal.get(j), EDGE_TYPE, (float) result.get(i, j), isDirectedGraph);
+                                graph.addEdge(ee);
+                            }
+                            ee.setWeight(result.get(i, j));
+                            ee.setAttribute(MMNT, inDimension + "<--->" + outDimension);
+                            ee.setLabel(inDimension + "-" + outDimension);
                         }
-                        
-                        ee.setWeight(result.get(i, j));
-                        ee.setAttribute(MMNT, inDimension + "<--->" + outDimension);
-                        ee.setLabel(inDimension + "-" + outDimension);
                     }
+		    if (cancelled) {
+		        return;
+		    }           
+		    if (online) Progress.progress (progressTicket,i+6);
                 }
             }
-        }
-        Progress.finish(progressTicket);
+            } else {
+            /*
+                AttributeColumn edgeStrengthCol = model.getEdgeTable().getColumn("MM-Strength");        
+                if ( edgeStrengthCol == null) {
+                     edgeStrengthCol = model.getEdgeTable().addColumn("MMStrength", AttributeType.FLOAT);
+                }
+
+                AttributeColumn nodeStrengthCol = model.getNodeTable().getColumn("MM-Connections");        
+                if ( nodeStrengthCol == null) {
+                     nodeStrengthCol = model.getNodeTable().addColumn("MM-Connections", AttributeType.FLOAT);
+                }
+               AttributeColumn nodeVolCol = model.getNodeTable().getColumn("MM-vol");        
+                if ( nodeVolCol == null) {
+                     nodeVolCol = model.getNodeTable().addColumn("MM-vol", AttributeType.FLOAT);
+                }
+            */ 
+           Table nodeTable = graphController.getGraphModel().getNodeTable();
+           Column edgeStrengthCol;
+            if(!edgeTable.hasColumn("MM-Strength")){
+                edgeStrengthCol = edgeTable.addColumn("MM-Strength", Float.class);
+            } else {
+                edgeStrengthCol = edgeTable.getColumn("MM-Strength");
+            } 
+ 
+            Column nodeStrengthCol;
+            if(!nodeTable.hasColumn("MM-Connections")){
+                nodeStrengthCol = nodeTable.addColumn("MM-Connections", Float.class);
+            } else {
+                nodeStrengthCol = nodeTable.getColumn("MM-Connections");
+            } 
+           Column nodeVolCol;
+            if(!nodeTable.hasColumn("MM-vol")){
+                nodeVolCol = nodeTable.addColumn("MM-vol", Float.class);
+            } else {
+                nodeVolCol = nodeTable.getColumn("MM-vol");
+            } 
+            
+            float minDim=(float)secondVertical.size();
+            for (int i = 0; i < result.getM(); i++) {
+                // for each node include the number of connections it has
+                // and the accumulated quadratic weights of them
+                Node node1 = firstVertical.get(i);
+                node1.setAttribute(nodeStrengthCol,new Float(100.0*firstUnweightWeights[i]/minDim));
+                node1.setAttribute(nodeVolCol, new Float(Math.sqrt(firstWeights[i])));
+                //firstVertical.get(i).getNodeData().getAttributes().setValue(nodeStrengthCol.getIndex(),(float)100.0*firstUnweightWeights[i]/minDim);
+                // firstVertical.get(i).getNodeData().getAttributes().setValue(nodeVolCol.getIndex(),(float)Math.sqrt(firstWeights[i]));
+                for (int j = 0; j < result.getN(); j++) {
+                    //System.out.println("processing (" + firstVertical.get(i).getNodeData().getLabel() +"-"+
+                    //        secondHorizontal.get(j).getNodeData().getLabel()+")");
+                    Node node2 = secondHorizontal.get(j);
+                    try {
+                    if (graph.contains(node1) && graph.contains(node2) 
+                            && graph.getEdge(node1, node2) == null ) {
+                                float iniWeight = (float) result.get(i, j); 
+                                //iniWeight= (float)(iniWeight/(Math.sqrt(secondWeights[j])*Math.sqrt(firstWeights[i]))) ; //that is the cosine distance
+                                iniWeight= (float)(100.0-200.0*Math.acos(iniWeight/(Math.sqrt(secondWeights[j])*Math.sqrt(firstWeights[i])))/Math.PI) ; //that is the cosine distance
+                                // now we weight it by the number of components
+                                float finalWeight= (float) 100.0* (float) Math.sqrt( resultUnw.get(i, j) / minDim) ; // how many components in common
+                                    // System.out.println("going to create");
+                                    if (iniWeight > threshold) {
+                                     ee = graphModel.factory().newEdge(node1, node2, EDGE_TYPE, iniWeight,this.directed );   
+                                     if (!ee.isSelfLoop()) {
+                                        ee.setAttribute(edgeStrengthCol,new Float(finalWeight));
+                                        ee.setAttribute(MMNT, inDimension + "<--->" + outDimension);
+                                        ee.setLabel(inDimension + "-" + outDimension);
+                                        graph.addEdge(ee);
+                                     }
+                                }
+                                //    else {System.out.println("under threshold "+iniWeight);}
+                      }
+                    } catch (Exception e){
+                        e.printStackTrace(System.out);
+                        System.out.println(e.getCause());
+                        break;
+                    }
+                      //else {System.out.println("not null?");}
+                }
+            if (cancelled) {
+                    return;
+             }
+            if (online) Progress.progress (progressTicket,i+6);
+            }
+        }     
+  
+        if (online) Progress.finish(progressTicket);
     }
     
     @Override
