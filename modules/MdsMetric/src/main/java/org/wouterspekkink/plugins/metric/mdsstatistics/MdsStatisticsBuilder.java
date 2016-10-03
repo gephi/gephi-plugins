6aebb5247b99a795303601a8aa4bfacb863b007b
Eduardo Ramos <eduramiba@gmail.com>
2016-10-03 20:34:02 +0200

    Fix code style details. Remove unnecessary jar. Fix long description.

--- modules/MdsMetric/src/main/java/org/wouterspekkink/plugins/metric/mdsstatistics/MdsStatisticsBuilder.java
+++ modules/MdsMetric/src/main/java/org/wouterspekkink/plugins/metric/mdsstatistics/MdsStatisticsBuilder.java
@@ -48,16 +48,12 @@ package org.wouterspekkink.plugins.metric.mdsstatistics;
  * 
  * @author wouter
  */
-
-
 import org.gephi.statistics.spi.Statistics;
 import org.gephi.statistics.spi.StatisticsBuilder;
 import org.openide.util.lookup.ServiceProvider;
 
 @ServiceProvider(service = StatisticsBuilder.class)
 public class MdsStatisticsBuilder implements StatisticsBuilder {
-    
-
     @Override
     public String getName() {
         return "Multidimensional Scaling";
