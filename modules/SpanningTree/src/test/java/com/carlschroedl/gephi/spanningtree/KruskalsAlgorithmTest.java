ef6237b2c52dfed2d2c53efcfa07afc380e258aa
Carl Schroedl <carlschroedl@gmail.com>
2016-11-05 20:29:51 -0500

    Broken checkpoint. Can't get ImportController#importFile() to return anything other than null.

--- modules/SpanningTree/src/test/java/com/carlschroedl/gephi/spanningtree/KruskalsAlgorithmTest.java
+++ modules/SpanningTree/src/test/java/com/carlschroedl/gephi/spanningtree/KruskalsAlgorithmTest.java
@@ -5,10 +5,14 @@
  */
 package com.carlschroedl.gephi.spanningtree;
 
+import org.gephi.desktop.importer.api.ImportControllerUI;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import javax.swing.JPanel;
+import org.gephi.graph.api.GraphController;
 import org.gephi.graph.api.GraphModel;
 import org.gephi.io.importer.api.Container;
 import org.gephi.io.importer.api.EdgeDirectionDefault;
@@ -20,9 +24,13 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.gephi.io.importer.api.ImportController;
+import org.gephi.io.importer.impl.ImportControllerImpl;
+import org.gephi.io.importer.spi.FileImporter;
+import org.gephi.io.importer.spi.FileImporterBuilder;
 import org.gephi.io.processor.plugin.DefaultProcessor;
 import org.gephi.project.api.ProjectController;
 import org.gephi.project.api.Workspace;
+import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 import org.openide.util.Utilities;
 
@@ -31,6 +39,8 @@ import org.openide.util.Utilities;
  * @author carlschroedl
  */
 public class KruskalsAlgorithmTest {
+    
+    GraphModel graphModel;
     
     public KruskalsAlgorithmTest() {
     }
@@ -44,7 +54,29 @@ public class KruskalsAlgorithmTest {
     }
     
     @Before
-    public void setUp() {
+public void setUp() {
+	ProjectController projectController = Lookup.getDefault().lookup(ProjectController.class);
+	projectController.newProject();
+	projectController.newWorkspace(projectController.getCurrentProject());
+        ImportControllerUI importControllerUi = Lookup.getDefault().lookup(ImportControllerUI.class);
+	ImportController importController = importControllerUi.getImportController();
+        
+	Container container;
+	try {
+		File file = new File(getClass().getResource("/com/carlschroedl/gephi/spanningtree/initial/wiki_kruskal_example.gephi").toURI());
+		InputStream is = getClass().getResourceAsStream("/com/carlschroedl/gephi/spanningtree/initial/wiki_kruskal_example.gephi");
+                FileImporter fi = importController.getFileImporter(file);
+                container = importController.importFile(is, fi);
+	}
+	catch (Exception ex) {
+		Exceptions.printStackTrace(ex);
+		return;
+	}
+
+	importController.process(container, new DefaultProcessor(), projectController.getCurrentWorkspace());
+
+	GraphController graphController = Lookup.getDefault().lookup(GraphController.class);
+	graphModel = graphController.getGraphModel();
     }
     
     @After
@@ -63,8 +95,13 @@ public class KruskalsAlgorithmTest {
         Workspace workspace = pc.getCurrentWorkspace();
 
         ImportController importController = Lookup.getDefault().lookup(ImportController.class);
+//        ImportController importController = new ImportControllerImpl();
         Container container;
+//        InputStream stream = getClass().getResourceAsStream("/com/carlschroedl/gephi/spanningtree/initial/wiki_kruskal_example.gephi");
         File file = Utilities.toFile(getClass().getResource("/com/carlschroedl/gephi/spanningtree/initial/wiki_kruskal_example.gephi").toURI());
+//        FileImporterBuilder fib = Lookup.getDefault().lookup(FileImporterBuilder.class);
+//        FileImporter fi = fib.buildImporter();
+//        container = importController.importFile(stream, fi);
         container = importController.importFile(file);
         container.getLoader().setEdgeDefault(EdgeDirectionDefault.DIRECTED);   //Force DIRECTED
         container.getLoader().setAllowAutoNode(false);  //Don't create missing nodes
