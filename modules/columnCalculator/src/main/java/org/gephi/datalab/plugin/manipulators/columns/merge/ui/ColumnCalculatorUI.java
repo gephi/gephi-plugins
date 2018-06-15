bf649cf1aab6b4e045ceabfbf2a5112383458375
U124283 <U124283@54007p35.ds.upf.edu>
2018-06-15 13:34:53 +0200

    Añadidos atributo y textfield con label. Pero aún estudiando com va la UI.

--- modules/columnCalculator/src/main/java/org/gephi/datalab/plugin/manipulators/columns/merge/ui/ColumnCalculatorUI.java
+++ modules/columnCalculator/src/main/java/org/gephi/datalab/plugin/manipulators/columns/merge/ui/ColumnCalculatorUI.java
@@ -46,19 +46,21 @@ import org.gephi.graph.api.Table;
 /**
  *
  * @author XGG3
+ * @editor oscarfont
  */
 public class ColumnCalculatorUI extends javax.swing.JPanel implements ManipulatorUI {
 
     private ColumnCalculator manipulator;
     private DialogControls dialogControls;
     private Table table;
+    private boolean correctFormula;
     
     /**
      * Creates new form ColumnCalculatorUI
      */
     public ColumnCalculatorUI() {
         initComponents();
-        titleTextField.getDocument().addDocumentListener(new DocumentListener() {
+        FormulaTextField.getDocument().addDocumentListener(new DocumentListener() {
 
             public void insertUpdate(DocumentEvent e) {
                 refreshOkButton();
@@ -75,8 +77,11 @@ public class ColumnCalculatorUI extends javax.swing.JPanel implements Manipulato
     }
     
     private void refreshOkButton() {
-        String text = titleTextField.getText();
-        dialogControls.setOkButtonEnabled(text != null && !text.isEmpty() && table != null && !table.hasColumn(text));//Title not empty and not repeated.
+        String text = FormulaTextField.getText();
+        //Here the formula should be parsed and checked if it is correct
+        //The parser should update the boolean value correctFormula dependig on if the formula is correct or not
+        correctFormula = true;
+        dialogControls.setOkButtonEnabled(text != null && !text.isEmpty() && table != null && !table.hasColumn(text) && correctFormula);//Title not empty and not repeated.
     }
     
     @Override
@@ -122,41 +127,38 @@ public class ColumnCalculatorUI extends javax.swing.JPanel implements Manipulato
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        titleTextField = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        FormulaTextField = new javax.swing.JTextField();
 
-        titleTextField.setText(org.openide.util.NbBundle.getMessage(ColumnCalculatorUI.class, "ColumnCalculatorUI.titleTextField.text")); // NOI18N
-        titleTextField.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                titleTextFieldActionPerformed(evt);
-            }
-        });
+        org.openide.awt.Mnemonics.setLocalizedText(jLabel1, org.openide.util.NbBundle.getMessage(ColumnCalculatorUI.class, "ColumnCalculatorUI.jLabel1.text")); // NOI18N
+
+        FormulaTextField.setText(org.openide.util.NbBundle.getMessage(ColumnCalculatorUI.class, "ColumnCalculatorUI.FormulaTextField.text")); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(223, Short.MAX_VALUE)
-                .addComponent(titleTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(109, 109, 109))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(35, 35, 35)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(FormulaTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(54, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(196, Short.MAX_VALUE)
-                .addComponent(titleTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(84, 84, 84))
+                .addContainerGap(195, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(FormulaTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(76, 76, 76))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void titleTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_titleTextFieldActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_titleTextFieldActionPerformed
-
-    
-
-
+   
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField titleTextField;
+    private javax.swing.JTextField FormulaTextField;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
