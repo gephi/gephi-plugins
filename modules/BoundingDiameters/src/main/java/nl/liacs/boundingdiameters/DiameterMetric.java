bd80092038eca08ab39257874810903aa91f502b
Eduardo Ramos <eduramiba@gmail.com>
2019-01-04 17:41:15 +0100

    Fix pull request problems #201

--- modules/BoundingDiameters/src/main/java/nl/liacs/boundingdiameters/DiameterMetric.java
+++ modules/BoundingDiameters/src/main/java/nl/liacs/boundingdiameters/DiameterMetric.java
@@ -1,11 +1,5 @@
 package nl.liacs.boundingdiameters;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.util.*;
 import org.gephi.graph.api.*;
 import org.gephi.statistics.plugin.ChartUtils;
