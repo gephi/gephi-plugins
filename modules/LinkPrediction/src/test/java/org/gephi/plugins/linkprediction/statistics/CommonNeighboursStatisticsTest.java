251c26fac08d7bf54fe319a730b5fb359849890b
Marco Romanutti <marco.romanutti@students.fhnw.ch>
2019-04-23 10:36:10 +0200

    Test directories added

---
+++ modules/LinkPrediction/src/test/java/org/gephi/plugins/linkprediction/statistics/CommonNeighboursStatisticsTest.java
@@ -0,0 +1,10 @@
+package org.gephi.plugins.linkprediction.statistics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommonNeighboursStatisticsTest {
+
+    @org.junit.jupiter.api.Test void execute() {
+
+    }
+}
