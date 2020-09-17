d79e0a27eb54b087cf0af4e04e97dbe503188c11
atopion <atopion@outlook.com>
2020-09-17 12:08:42 +0200

    Initial commit

---
+++ modules/Inspector/README.md
@@ -0,0 +1,4 @@
+## Inspector
+
+This README supports Markdown, see [syntax](https://help.github.com/articles/markdown-basics/)
+
---
+++ modules/Inspector/src/main/java/com/atopion/gephi/inspector/InspectorAction.java
@@ -0,0 +1,225 @@
+/*
+ * Copyright 2020 atopion.com
+ * Authors : Timon Vogt
+ * Website : https://www.atopion.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.atopion.gephi.inspector;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.geom.RoundRectangle2D;
+import java.util.List;
+import java.util.Set;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import org.gephi.graph.api.Node;
+import org.gephi.visualization.VizController;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle.Messages;
+import org.openide.util.actions.BooleanStateAction;
+
+@ActionID(
+        category = "Tools",
+        id = "com.atopion.gephi.inspector.InspectorAction"
+)
+@ActionRegistration(
+        displayName = "#CTL_InspectorAction"
+)
+@ActionReferences({
+    @ActionReference(path = "Menu/Tools", position = 0),
+    @ActionReference(path = "Shortcuts", name = "DO-I")
+})
+@Messages("CTL_InspectorAction=Inspector")
+public final class InspectorAction extends BooleanStateAction {
+
+    public InspectorAction() {
+        super();
+        
+        // Initial state is false
+        setBooleanState(false);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        setBooleanState(!getBooleanState());
+        
+        // Plugins main method
+        toggleInspector();
+    }
+
+    @Override
+    public String getName() {
+        return "Inspector";
+    }
+
+    @Override
+    public HelpCtx getHelpCtx() {
+        return HelpCtx.DEFAULT_HELP;
+    }
+    
+    @Override
+    public JMenuItem getMenuPresenter() {
+        return new org.openide.awt.Actions.CheckboxMenuItem(this, false);
+    }
+    
+    
+    
+    private Timer timer = null;
+    
+    private void toggleInspector() {
+        final int delay = 500; // Timer run delay in ms. Shorter value means more CPU load but more reactive UI.
+        
+        // The user has just activated the inspector.
+        if(getBooleanState()) {
+            timer = new Timer(delay, runner);
+            timer.start();
+        }
+        
+        // The user has just deactivated the inspector
+        else {
+            nodePropertiesFrame.remove(); // To make sure it isn't stuck.
+            timer.stop();
+            timer = null;
+        }
+    }
+    
+    private final ActionListener runner = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // Query the currently selected Nodes
+            List<Node> nodes = VizController.getInstance().getSelectionManager().getSelectedNodes();
+            
+            // If the mouse currently hovers over a node, only one is selected.
+            // In all other cases, remove the frame and end.
+            if(nodes.size() != 1) {
+                nodePropertiesFrame.remove();
+                return;
+            }
+            
+            // The mouse currently hovers over a node.
+            // First, calculate the mouse location.
+            int[] mousePosition = calculateMouseLocation();
+            
+            // Then display the nodePropertiesFrame using the currently selected node.
+            // The window will be located 20px left and 20px down of the mouse.
+            nodePropertiesFrame.show(nodes.get(0), mousePosition[0] + 20, mousePosition[1] + 20);
+        }
+        
+        
+        private int[] calculateMouseLocation() {
+            // Since javax.swing measures the y coordinate top - down and 
+            // Gephi's VizController measures it bottom - up, we need to 
+            // convert the y coordinate using Gephi's viewportHeight.
+            
+            // Additionally, the mouse position is relative to Gephi's drawable,
+            // therefore we need its location on screen.
+            
+            int[] mousePosition = new int[2];
+            
+            float[] mouse = VizController.getInstance().getGraphIO().getMousePosition();
+            Point screenLocation = VizController.getInstance().getDrawable().getLocationOnScreen();
+            int viewportHeight = VizController.getInstance().getDrawable().getViewportHeight();
+            
+            // The x coordinate
+            mousePosition[0] = screenLocation.x + (int) mouse[0];
+            
+            // The y coordinate
+            mousePosition[1] = screenLocation.y + (viewportHeight - (int) mouse[1]);
+            
+            return mousePosition;
+        }
+    };
+    
+    private final NodePropertiesFrame nodePropertiesFrame = new NodePropertiesFrame();
+    
+    // The node properties will be displayed in their own frame.
+    class NodePropertiesFrame extends JFrame {
+        private JPanel contentPanel;
+        
+        public NodePropertiesFrame() {
+            super();
+            this.setType(Type.UTILITY);
+            this.setUndecorated(true);
+            this.setAlwaysOnTop(true);
+            this.getRootPane().putClientProperty("apple.awt.draggableWindowBackground", false);
+            this.setOpacity(0.75f);
+            this.setVisible(false);
+            
+            this.addComponentListener(new ComponentAdapter() {
+                @Override
+                public void componentResized(ComponentEvent e) {
+                    setShape(new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), 10, 10));
+                }
+            });
+            
+            contentPanel = new JPanel();
+            contentPanel.setLayout(new GridLayout(0, 2));
+            contentPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+            contentPanel.setBackground(new Color(0, 0, 0));
+            
+            this.getContentPane().setLayout(new BorderLayout());
+            this.getContentPane().add(contentPanel, BorderLayout.CENTER);
+        }
+        
+        // Makes the frame invisible
+        public void remove() {
+            this.setVisible(false);
+        }
+        
+        // Updates the frame's content with a new node and displays it at a given location.
+        public void show(Node node, int x, int y) {
+            this.contentPanel.removeAll();
+            Set<String> keys = node.getAttributeKeys();
+            
+            // For each attribute of the node, one row will be displayed,
+            // containing the column name and the nodes value (or "null" if none).
+            for(String key : keys) {
+                Object attr = node.getAttribute(key);
+                JLabel keyLabel = new JLabel(key + " : ");
+                JLabel dataLabel = new JLabel(attr != null ? attr.toString() : "null");
+                
+                keyLabel.setForeground(Color.WHITE);
+                keyLabel.setFont(new Font("Arial", Font.BOLD, 13));
+                
+                dataLabel.setForeground(Color.WHITE);
+                dataLabel.setFont(new Font("Arial", Font.PLAIN, 13));
+                
+                this.contentPanel.add(keyLabel);
+                this.contentPanel.add(dataLabel);
+            }
+            
+            // The GridLayoutManager will make the window just as big as it
+            // needs to be.
+            this.pack();
+            this.setLocation(x, y);
+            this.setVisible(true);
+        }
+    }
+}
