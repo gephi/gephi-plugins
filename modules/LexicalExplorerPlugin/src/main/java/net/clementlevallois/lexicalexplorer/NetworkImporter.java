e0c7b6d86a7a2ad153c3e7cdf5f22a98e2aefb50
Clement Levallois <1244100+seinecle@users.noreply.github.com>
2023-01-04 16:04:29 +0100

    continued working on the tokenization and ranking of most frequent terms

--- modules/LexicalExplorerPlugin/src/main/java/net/clementlevallois/lexicalexplorer/NetworkImporter.java
+++ modules/LexicalExplorerPlugin/src/main/java/net/clementlevallois/lexicalexplorer/NetworkImporter.java
@@ -14,10 +14,13 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import net.clementlevallois.stopwords.StopWordsRemover;
 import net.clementlevallois.umigon.model.TextFragment;
 import net.clementlevallois.umigon.model.TypeOfTextFragment.TypeOfTextFragmentEnum;
 import net.clementlevallois.umigon.tokenizer.controller.UmigonTokenizer;
+import net.clementlevallois.utils.Multiset;
 import org.gephi.graph.api.Column;
 import org.gephi.graph.api.Graph;
 import org.gephi.graph.api.GraphController;
@@ -39,11 +42,11 @@ import org.openide.util.Lookup;
  */
 public class NetworkImporter {
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws IOException, Exception {
         new NetworkImporter().importFromFile();
     }
 
-    public void importFromFile() throws IOException {
+    public void importFromFile() throws IOException, Exception {
 
         Path exampleGexf = Path.of("G:\\Mon Drive\\Twitch stream\\gephi plugin development\\gephi-plugins\\modules\\LexicalExplorerPlugin\\qatar user network.gexf");
 
@@ -60,6 +63,7 @@ public class NetworkImporter {
         //Import file
         Container container;
         FileImporter fi = new ImporterGEXF();
+
         //Append imported data to GraphAPI
         container = importController.importFile(new StringReader(gexfFileAsString), fi);
         container.closeLoader();
@@ -93,18 +97,47 @@ public class NetworkImporter {
 
         System.out.println("number of nodes that have a description: " + descriptions.size());
 
-        List<TextFragment> textFragments = new ArrayList();
+        Multiset<String> textFragments = new Multiset();
         Set<String> languageSpecificLexicon = new HashSet();
+
+        /*
+        
+        1. we loop through the list of textual descriptions and tokenize them
+        
+        2. The result is a list of TextFragment objects
+        
+        3. We keep only the text fragments that are not punctuation signs nor whitespaces
+
+         */
+        StopWordsRemover stopWordsRemoverEN = new StopWordsRemover(3, "en");
+        StopWordsRemover stopWordsRemoverES = new StopWordsRemover(3, "es");
 
         for (String description : descriptions) {
             List<TextFragment> textFragmentsForOneDescription = UmigonTokenizer.tokenize(description, languageSpecificLexicon);
-            for (TextFragment oneTextFragment: textFragmentsForOneDescription){                
-                if (oneTextFragment.getTypeOfTextFragmentEnum()!= TypeOfTextFragmentEnum.WHITE_SPACE & oneTextFragment.getTypeOfTextFragmentEnum()!= TypeOfTextFragmentEnum.PUNCTUATION){
-                    textFragments.add(oneTextFragment);
-                }                
+            for (TextFragment oneTextFragment : textFragmentsForOneDescription) {
+                if (
+                        oneTextFragment.getTypeOfTextFragmentEnum() != TypeOfTextFragmentEnum.WHITE_SPACE
+                        & oneTextFragment.getTypeOfTextFragmentEnum() != TypeOfTextFragmentEnum.PUNCTUATION
+                        & !stopWordsRemoverEN.shouldItBeRemoved(oneTextFragment.getOriginalForm())
+                        & !stopWordsRemoverES.shouldItBeRemoved(oneTextFragment.getOriginalForm())
+                        & oneTextFragment.getOriginalForm().length() > 4
+                        ) {
+                    textFragments.addOne(oneTextFragment.getOriginalForm());
+                }
             }
-        }        
-        System.out.println("number of text fragments in the description: " + textFragments.size());
+        }
+        System.out.println("number of text fragments in the description: " + textFragments.getSize());
+
+        List<Map.Entry<String, Integer>> multisetRankedFromTopFrequency = textFragments.sortDesc(textFragments);
+
+        int i = 0;
+        for (Map.Entry<String, Integer> tf : multisetRankedFromTopFrequency) {
+            System.out.println("top term: " + tf.getKey() + " (appearing " + tf.getValue() + " times).");
+            if (i++ > 10) {
+                break;
+            }
+        }
+
     }
 
 }
