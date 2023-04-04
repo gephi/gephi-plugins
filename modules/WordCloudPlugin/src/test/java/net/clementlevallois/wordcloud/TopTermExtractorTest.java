d85426a69997432dcbfa47b2d869b9bce85550dd
seinecle <1244100+seinecle@users.noreply.github.com>
2023-04-04 12:53:24 +0200

    added a test for stopword removals in different languages. Updated the test gexf for this

--- modules/WordCloudPlugin/src/test/java/net/clementlevallois/wordcloud/TopTermExtractorTest.java
+++ modules/WordCloudPlugin/src/test/java/net/clementlevallois/wordcloud/TopTermExtractorTest.java
@@ -21,8 +21,9 @@ public class TopTermExtractorTest {
         GraphModel graphModel = GraphImporter.importGraph(TopTermExtractorTest.class, "terms.gexf");
 
         TopTermExtractor ttt = new TopTermExtractor();
-        Assert.assertTrue(ttt.tokenizeSelectedTextualAttributeForTheEntireGraph(graphModel, "terms", "en"));
-        Assert.assertEquals(Set.of("gephi"),
+        List<String> selectedLanguages = null;
+        Assert.assertTrue(ttt.tokenizeSelectedTextualAttributeForTheEntireGraph(graphModel, "terms", selectedLanguages));
+        Assert.assertEquals(Set.of("gephi", "pendant", "actuellement", "allez"),
                 new HashSet<>(DataManager.getMapOfNodeIdsToTheirTextFragments().values().stream().reduce((l1, l2) -> {
                     l1.addAll(l2);
                     return l1;
@@ -35,20 +36,38 @@ public class TopTermExtractorTest {
         GraphModel graphModel = GraphImporter.importGraph(TopTermExtractorTest.class, "terms.gexf");
 
         TopTermExtractor ttt = new TopTermExtractor();
-        ttt.tokenizeSelectedTextualAttributeForTheEntireGraph(graphModel, "terms", "en");
+        List<String> selectedLanguages = null;
+        ttt.tokenizeSelectedTextualAttributeForTheEntireGraph(graphModel, "terms", selectedLanguages);
         List<Node> selectedNodes = new ArrayList();
         NodeIterable nodes = graphModel.getGraph().getNodes();
-        Iterator<Node> it = nodes.iterator();
-        while (it.hasNext()) {
-            Node next = it.next();
+        for (Node next : nodes) {
             if (next.getLabel().equals("Node 3")) {
                 selectedNodes.add(next);
             }
         }
-        String topTermsExtractorFromSelectedNodes = ttt.topTermsExtractorFromSelectedNodes(selectedNodes, 3);
+        String topTermsExtractorFromSelectedNodes = ttt.topTermsExtractorFromSelectedNodes(selectedNodes, 5);
         Assert.assertTrue(topTermsExtractorFromSelectedNodes.contains("gephi"));
         Assert.assertTrue(!topTermsExtractorFromSelectedNodes.contains("hello"));
         Assert.assertTrue(!topTermsExtractorFromSelectedNodes.contains("world"));
+
+    }
+    @Test
+    public void testStopWordsRemovalOnSeveralLanguages() {
+        GraphModel graphModel = GraphImporter.importGraph(TopTermExtractorTest.class, "terms.gexf");
+
+        TopTermExtractor ttt = new TopTermExtractor();
+        List<String> selectedLanguages = List.of("en", "fr");
+        ttt.tokenizeSelectedTextualAttributeForTheEntireGraph(graphModel, "terms", selectedLanguages);
+        List<Node> selectedNodes = new ArrayList();
+        NodeIterable nodes = graphModel.getGraph().getNodes();
+        for (Node next : nodes) {
+                selectedNodes.add(next);
+        }
+        String topTermsExtractorFromSelectedNodes = ttt.topTermsExtractorFromSelectedNodes(selectedNodes, 5);
+        Assert.assertTrue(!topTermsExtractorFromSelectedNodes.contains("actuellement"));
+        Assert.assertTrue(!topTermsExtractorFromSelectedNodes.contains("allez"));
+        Assert.assertTrue(!topTermsExtractorFromSelectedNodes.contains("pendant"));
+        Assert.assertTrue(topTermsExtractorFromSelectedNodes.contains("gephi"));
 
     }
 }
